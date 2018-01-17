1a7cd9d45b7fa6ff9bdcf7c3835578802c16faf1
Master Laptop <tech.storm@stjlabs.com>
2018-01-17 13:00:20 -0500

    Fixed Formatting on PathfindingPoint2D

--- Pathfinding Code/src/classes/PathfinderPoint2D.java
+++ Pathfinding Code/src/classes/PathfinderPoint2D.java
@@ -12,6 +12,9 @@ public class PathfinderPoint2D{
 	double bA, bB;
 	final double SAFE_DIST = 5;
 	boolean positiveLast = false;
+	
+	
+	//Checks for an intersection with an obstacle and returns the index in the array of said obstacle. 
         public int checkObsInts(ArrayList<Line2D.Double> a, Line2D.Double path) {
         	for(Line2D.Double line : a) {
         		if(path.intersectsLine(line)) {
@@ -23,6 +26,7 @@ public class PathfinderPoint2D{
         	return 0;
         }
         
+    //Returns the X of the intersection of two lines.
         public double getXofIntersect(Line2D.Double l1, Line2D.Double l2) {
         	y1a = l1.getY1();
         	y2a = l1.getY2();
@@ -40,6 +44,8 @@ public class PathfinderPoint2D{
         	return (bB-bA)*(mA-mB);
         	
         }
+        
+     //Returns the Y of the intersection of two lines. 
         public double getYofIntersect(Line2D.Double l1, Line2D.Double l2) {
         	double xCoord = getXofIntersect(l1,l2);
         	
@@ -49,14 +55,20 @@ public class PathfinderPoint2D{
         	
         }
         
+     //Returns the delta x to step along a circular arc given radius 1 and angle theta
         public double getDeltaXForArc(double startAngle, double theta) {
         	//DeltaX = -rSin(theta - pi/2 + startAngle)
         	return -Math.toDegrees(Math.sin(Math.toRadians(theta - 90 + startAngle)));
         }
+        
+        
+     //Returns the delta y to step along a circular arc given radius 1 and angle theta
         public double getDeltaYForArc(double startAngle, double theta) {
         	//DeltaY = rCos(theta - pi/2 + startAngle)
         	return Math.toDegrees(Math.cos(Math.toRadians(theta - 90 + startAngle)));
         }
+        
+     //Returns a new line which is on the same slope as the given line, but with an adjusted distance.
         public Line2D.Double lineOnSlopeForDistance(Line2D.Double path, double distance) {
         	double theta = angleFromLine(path, 0);
         	double xStep = distance*(Math.cos(theta));
@@ -65,6 +77,8 @@ public class PathfinderPoint2D{
         	Line2D.Double newPath = new Line2D.Double(path.getP1(), new Point2D.Double(path.getX1() + xStep, path.getY1() + yStep));
         	return newPath;
         }
+        
+     //Returns a new Point2D in double precision which is a specified safe distance back from an obs intersect.
         public Point2D.Double safePlace(Line2D.Double path, Line2D.Double obs){
         	
         	double theta = angleFromLine(path, 0);
@@ -74,6 +88,8 @@ public class PathfinderPoint2D{
         	return new Point2D.Double(getXofIntersect(path, obs)-xStepBack, getYofIntersect(path, obs)-yStepBack);
         }
         
+        
+     //Returns a new Line2D which is the line which most efficiently avoids an obstacle.
         public Line2D.Double safeLine(double OrigTheta, Point2D.Double origin, Point2D.Double goalCopy, Line2D.Double obs){
 			int thetaWorkingPositive = 0;
 			int thetaWorkingNegative = 0;
@@ -108,39 +124,41 @@ public class PathfinderPoint2D{
         	
         	
         }
-    
+        
+        
+    //Returns the angle of a line from the x axis or a specified original angle.
         public double angleFromOG(Point2D origin, Point2D goal, double a) {
 		
-		double deltaY = goal.getY() - origin.getY();
-		double deltaX = goal.getX() - origin.getX();
-		double output = 0;
+        	double deltaY = goal.getY() - origin.getY();
+        	double deltaX = goal.getX() - origin.getX();
+        	double output = 0;
           
-		if(deltaX > 0) {
-			output = Math.atan(deltaY/deltaX)-a;
-		}
+        	if(deltaX > 0) {
+        		output = Math.atan(deltaY/deltaX)-a;
+        	}
 		
-		else if(deltaX < 0) {
-			output = 180+Math.atan(deltaY/deltaX)-a;
-		}
+        	else if(deltaX < 0) {
+        		output = 180+Math.atan(deltaY/deltaX)-a;
+        	}
 		
-		else if(deltaX == 0) {
-			if(deltaY > 0) {
-				output = 90-a;
-			} else if (deltaY < 0) {
-				output = 270-a;
-			}
-		}
+        	else if(deltaX == 0) {
+        		if(deltaY > 0) {
+        			output = 90-a;
+        		} else if (deltaY < 0) {
+        			output = 270-a;
+        		}
+        	}
 		
-		return output;
-	}
-    
+        	return output;
+        }
+    //Alternate header for angleFromOG()
         public double angleFromLine(Line2D.Double line, double a) {
-		return angleFromOG(line.getP1(),line.getP2(),a);
-	}
+        	return angleFromOG(line.getP1(),line.getP2(),a);
+        }
 	
-        //This method assumes the angle is already right. use the above method to accomplish that.
+   //This method assumes the angle is already right. use the above method to accomplish that.
         public double distanceToGo(Point2D origin, Point2D goal ) {
-		return origin.distance(goal);
-	}
+        	return origin.distance(goal);
+        }
 	
 }
